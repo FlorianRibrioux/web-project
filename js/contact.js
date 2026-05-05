5615d3c629ecf5b213c1a1e2501443266d525a5e
FlorianRibrioux <florian.ribrioux@efrei.net>
2026-05-05 17:52:52 +0200

    Modified the way contact form sending work

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,20 @@
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+}
+
 submitInformation = function() {
+    if (document.getElementById("name").value == "" 
+    || document.getElementById("surname").value == ""
+    || document.getElementById("email").value == ""
+    || document.getElementById("message").value == "") {
+        return;
+    }
+
+    if (!validateEmail(document.getElementById("email").value)) {
+        return;
+    }
+
     alert("Thank you for contacting us! We will get back to you as soon as possible.");
 }
+
