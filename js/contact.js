220dd846fde7eb818fcdacd9051c385d2ba7efa5
FlorianRibrioux <florian.ribrioux@efrei.net>
2026-05-06 13:57:06 +0200

    Perfected the contact form

--- js/contact.js
+++ js/contact.js
@@ -3,7 +3,9 @@ function validateEmail(email) {
     return re.test(String(email).toLowerCase());
 }
 
-submitInformation = function() {
+submitInformation = function(event) {
+    event.preventDefault();
+
     if (document.getElementById("name").value == "" 
     || document.getElementById("surname").value == ""
     || document.getElementById("email").value == ""
@@ -12,9 +14,12 @@ submitInformation = function() {
     }
 
     if (!validateEmail(document.getElementById("email").value)) {
+        alert("Please enter a valid email address.");
+        document.getElementById("email").focus();
         return;
     }
 
     alert("Thank you for contacting us! We will get back to you as soon as possible.");
+    document.getElementById("contact-form").reset();
 }
 
